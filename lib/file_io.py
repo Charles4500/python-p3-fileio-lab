9159de7cad3197ff6f30c1fe3c706269c902f769
CharlesKibet <charlesbiegon973@gmail.com>
2024-06-02 11:45:54 +0300

    Import lab done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+   with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
 
 def append_file(file_name, append_content):
-    pass
+      with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
 
 def read_file(file_name):
-    pass
+     with open(f'{file_name}.txt') as f:
+        return f.read()
+read_file(file_name="logfile")
